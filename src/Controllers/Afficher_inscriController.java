c6b4565abf983bcba451fc5e3bf55eb37106f125
Maissa Loussaief <maissa.loussaief@esprit.tn>
2021-03-08 21:18:39 +0100

    interface afficher

--- src/Controllers/Afficher_inscriController.java
+++ src/Controllers/Afficher_inscriController.java
@@ -7,7 +7,9 @@ package Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 
 /**
  * FXML Controller class
@@ -15,6 +17,17 @@ import javafx.fxml.Initializable;
  * @author Tarek.Loussaief
  */
 public class Afficher_inscriController implements Initializable {
+
+    @FXML
+    private TableColumn<?, ?> id_inscri;
+    @FXML
+    private TableColumn<?, ?> nom_utilisateur;
+    @FXML
+    private TableColumn<?, ?> nom_certificat;
+    @FXML
+    private TableColumn<?, ?> description;
+    @FXML
+    private TableColumn<?, ?> domaine;
 
     /**
      * Initializes the controller class.
